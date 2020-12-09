acee94c26eba0635036dda0ce4a77f4cae1daf85
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2020-12-08 20:37:33 -0500

    Added inquirer prompt that runs specific functions based on answers

--- employeeTracker.js
+++ employeeTracker.js
@@ -17,6 +17,29 @@ const connection = mysql.createConnection({
     init();
   });
 
+
   function init(){
-      console.log("Connected!");
-  }
+    inquirer
+    .prompt({
+      name: "todo",
+      type: "list",
+      message: "What would you like to do?",
+      choices: ["View all Employees", "Add Employee","Exit"]
+    })
+    .then(function(answer) {
+        if (answer.todo === "View all Employees") {
+            viewEmployees();
+          }
+          else if(answer.todo === "Add Employee") {
+            addEmployee();
+          } 
+          else{
+            connection.end();
+          }
+        });
+    }
+  
+    function viewEmployees (){};
+    function addEmployee (){};
+
+
