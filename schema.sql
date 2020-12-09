0a5aad8580046ee7ac173e140d387e5f22879e5f
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2020-12-08 19:08:15 -0500

    Added columns for employee table

--- schema.sql
+++ schema.sql
@@ -4,16 +4,22 @@ CREATE database employeesDB;
 USE employeesDB;
 
 CREATE TABLE department(
-    id INT PRIMARY KEY,
-    name VARCHAR(30)
+    id INT PRIMARY KEY NOT NULL,
+    name VARCHAR(30) NULL
 );
 
 CREATE TABLE role(
-    id INT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT 
+    id INT PRIMARY KEY NOT NULL,
+    title VARCHAR(30) NULL,
+    salary DECIMAL NULL,
+    department_id INT NULL 
 );
 
-CREATE TABLE employee();
+CREATE TABLE employee(
+    id - INT PRIMARY KEY NULL,
+    first_name VARCHAR(30) NULL, 
+    last_name VARCHAR(30) NULL, 
+    role_id INT NULL, 
+    manager_id INT NULL
+);
 
