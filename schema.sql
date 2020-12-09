4ac10c23f9657c98d96ec28b2f1263b13feea05f
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2020-12-08 20:32:52 -0500

    added auto increment to id's

--- schema.sql
+++ schema.sql
@@ -4,22 +4,21 @@ CREATE database employees_DB;
 USE employees_DB;
 
 CREATE TABLE department(
-    id INT PRIMARY KEY NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     name VARCHAR(30) NULL
 );
 
 CREATE TABLE role(
-    id INT PRIMARY KEY NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     title VARCHAR(30) NULL,
     salary DECIMAL NULL,
     department_id INT NULL 
 );
 
 CREATE TABLE employee(
-    id INT PRIMARY KEY NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     first_name VARCHAR(30) NULL, 
     last_name VARCHAR(30) NULL, 
     role_id INT NULL, 
     manager_id INT NULL
 );
-
