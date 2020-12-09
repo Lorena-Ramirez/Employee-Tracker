c18de0932aa8eb94119c632fb24245ba74c216ff
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2020-12-08 20:33:15 -0500

    updated dummy data to have null

--- seed.sql
+++ seed.sql
@@ -1,23 +1,22 @@
 USE employees_DB;
 
-INSERT INTO department(name)
+INSERT INTO department (name)
 VALUES ("sales"),
 ("engineering"),
 ("legal");
 
-INSERT INTO roles(title, salary, department_id)
+INSERT INTO role (title, salary, department_id)
 VALUES ("Sales Lead",100000,1),
 ("Salesperson",80000,1),
 ("Lead Engineer",150000,2),
 ("Software Engineer",120000,2),
 ("Legal Team Lead",250000,3),
-("Lawyyer",190000,3),
-;
+("Lawyer",190000,3);
 
-INSERT INTO employees(first_name,last_name,role_id,manager_id)
-VALUES ("Leanna", "Smith", 1),
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ("Leanna", "Smith", 1, null),
  ("John", "Doe", 2 , 1),
- ("Julia", "Baker", 3,),
+ ("Julia", "Baker", 3, null),
  ("Matt", "Reynolds", 4, 3),
- ("Tina", "Bay", 5),
- ("Ryan", "Paynter", 6, 5),
+ ("Tina", "Bay", 5, null),
+ ("Ryan", "Paynter", 6, 5);
